642a7392d07ae0284292eed1f522957fd556ef58
Nikhil Kumar <nikhilkumarsingh166@gmail.com>
2022-06-23 11:44:16 +0530

    digit-sum added

---
+++ recursion-and-backtracking/digit-sum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum( int n){
+    if( n == 0){ //base condition to stop the recurison
+        return 0;
+    }
+
+    else {
+        return sum(n/10) + (n%10);
+    }
+}
+
+int main(){
+
+    int num;
+    cin>>num;
+    int result = sum(num);
+    cout<<result<<endl;
+
+    return 0;
+}
